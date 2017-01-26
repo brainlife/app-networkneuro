45ab9ef0fd17a639ddb84d4a1d60a2a99c9926b1
Soichi Hayashi <hayashis@login2.bigred2.uits.iu.edu>
2017-01-26 09:56:43 -0500

    bootstrapped preprocess.sh

---
+++ preprocess.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#pull fsdir from config.json (TODO)
+fsdir = "/N/u/hayashis/Karst/testdata/sca-service-networkneuro/freesurfer"
+
+....magic....
+
+mkdir labels
+echo "stuff" > labels.something
+
