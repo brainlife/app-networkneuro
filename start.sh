72a0224638d3b59a8c1bd1e4413461861aa3947e
Soichi Hayashi <hayashis@iu.edu>
2017-08-03 10:03:06 -0400

    making qsub the last command to run

--- start.sh
+++ start.sh
@@ -6,6 +6,5 @@ if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 #just in case..
 rm -f finished
 
-jobid=`qsub $SERVICE_DIR/submit.pbs`
-echo $jobid > jobid
+qsub $SERVICE_DIR/submit.pbs > jobid
 
