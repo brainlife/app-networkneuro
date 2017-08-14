8622c0bfd523b622f9a0ae7a6b1f86c718ba132e
Brent McPherson <bcmcpher@iu.edu>
2017-08-14 12:11:48 -0400

    updated service to work with latest version of toolbox

--- main.m
+++ main.m
@@ -2,6 +2,7 @@ function [] = main()
 
 disp('loading paths')
 addpath(genpath('/N/u/hayashis/BigRed2/git/encode'))
+addpath(genpath('/N/u/hayashis/BigRed2/git/fine'))
 addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
 addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
 addpath(genpath('/N/u/hayashis/BigRed2/git/mba'))
@@ -9,86 +10,52 @@ addpath(genpath('/N/u/hayashis/BigRed2/git/mba'))
 % load my own config.json
 config = loadjson('config.json');
 
-% run the network generation process - condensed to a funcion
-% using 32 cores seems to cause OOM
-[ emat, cmat, pconn, out, nmat, imat ] = feMatrixFromTensor_clean(config.fe, 'rois', config.cachedir, 16);
+% create the labels
+labels = fsInflateDK(config.labels, 3, 'vert', 'aparc+aseg_labels.nii.gz');
+
+% run the network generation process
+[ pconn, rois, omat, olab ] = fnBuildNetworks(config.fe, labels, config.fa, config.output, 4, config.cache)
 
 % save the outputs
 mkdir('output');
-save('output/emat.mat', 'emat');
-save('output/cmat.mat', 'cmat');
+save('output/omat.mat', 'omat');
+save('output/olab.mat', 'olab');
 save('output/pconn.mat', 'pconn');
-save('output/out.mat', 'out');
-save('output/mat.mat', 'nmat');
-save('output/imat.mat', 'imat');
+save('output/rois.mat', 'rois');
 
 %% create and save some plots
 
 % uncleaned streamline count
 figure();
 colormap('hot');
-imagesc(log(emat(:,:,2)));
+imagesc(log10(omat(:,:,2)));
 axis('square'); axis('equal'); axis('tight');
 title('Log Streamline Density');
 xlabel('FS DK Regions');
 ylabel('FS DK Regions');
 y = colorbar;
-ylabel(y, 'Log Density of Streamlines');
+ylabel(y, 'Log_{10} Density of Streamlines');
 set(gca, 'XTickLabel', '', 'YTickLabel', '', 'XTick', [], 'YTick', []);
 line([34.5 34.5], [0.5 68.5], 'Color', [0 0 1]);
 line([0.5 68.5], [34.5 34.5], 'Color', [0 0 1]);
 line([68.5 0.5], [68.5 0.5], 'Color', [0 0 1]);
-saveas(gcf, './uncleaned_edge_density.png');
+saveas(gcf, './edge_density.png');
 close all;
 
 % cleaned streamline count
 figure();
 colormap('hot');
-imagesc(log(cmat(:,:,2)));
+imagesc(log10(omat(:,:,10)));
 axis('square'); axis('equal'); axis('tight');
-title('Cleaned Log Streamline Density');
+title('Log_{10} LiFE');
 xlabel('FS DK Regions');
 ylabel('FS DK Regions');
 y = colorbar;
-ylabel(y, 'Log Density of Streamlines');
+ylabel(y, 'Log of LiFE EMD');
 set(gca, 'XTickLabel', '', 'YTickLabel', '', 'XTick', [], 'YTick', []);
 line([34.5 34.5], [0.5 68.5], 'Color', [0 0 1]);
 line([0.5 68.5], [34.5 34.5], 'Color', [0 0 1]);
 line([68.5 0.5], [68.5 0.5], 'Color', [0 0 1]);
-saveas(gcf, './cleaned_edge_density.png');
-close all;
-
-% uncleaned EMD
-figure();
-colormap('hot');
-imagesc(log(emat(:,:,10)));
-axis('square'); axis('equal'); axis('tight');
-title('EMD');
-xlabel('FS DK Regions');
-ylabel('FS DK Regions');
-y = colorbar;
-ylabel(y, 'Log Scanner Units');
-set(gca, 'XTickLabel', '', 'YTickLabel', '', 'XTick', [], 'YTick', []);
-line([34.5 34.5], [0.5 68.5], 'Color', [0 0 1]);
-line([0.5 68.5], [34.5 34.5], 'Color', [0 0 1]);
-line([68.5 0.5], [68.5 0.5], 'Color', [0 0 1]);
-saveas(gcf, './uncleaned_edge_emd.png');
-close all;
-
-% cleaned EMD
-figure();
-colormap('hot');
-imagesc(log(cmat(:,:,10)));
-axis('square'); axis('equal'); axis('tight');
-title('Cleaned EMD');
-xlabel('FS DK Regions');
-ylabel('FS DK Regions');
-y = colorbar;
-ylabel(y, 'Log Scanner Units');
-set(gca, 'XTickLabel', '', 'YTickLabel', '', 'XTick', [], 'YTick', []);
-line([34.5 34.5], [0.5 68.5], 'Color', [0 0 1]);
-line([0.5 68.5], [34.5 34.5], 'Color', [0 0 1]);
-line([68.5 0.5], [68.5 0.5], 'Color', [0 0 1]);
-saveas(gcf, './cleaned_edge_emd.png');
+saveas(gcf, './edge_LiFE.png');
 close all;
 
