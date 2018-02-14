54eeeb5a8cc44cc6d85dcd0498b28fa6abcdcd03
bcmcpher <bcmcpher@iu.edu>
2018-02-14 11:40:48 -0500

    added freesurfer license to container

--- preprocess.sh
+++ preprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## load singularity
-module load singularity 2> /dev/null
+#module load singularity 2> /dev/null
 
 ## parse argument for path to file
 export fsdir=`$SERVICE_DIR/jq -r '.fsdir' config.json`
@@ -13,6 +13,7 @@ fmgz=$fsdir/mri/aparc+aseg.mgz
 echo Converting $fmgz into a .nii.gz now...
 
 ## run file conversion docker
-singularity exec -e docker://brainlife/freesurfer mri_convert $fmgz --out_orientation RAS aparc+aseg.nii.gz
+#singularity exec -e docker://brainlife/freesurfer mri_convert $fmgz --out_orientation RAS aparc+aseg.nii.gz
+mri_convert $fmgz --out_orientation RAS aparc+aseg.nii.gz
 
 echo Done converting labels.
