9c8a50f69800f330c6dd9c8f81ef7cd040daaa32
Soichi Hayashi <hayashis@iu.edu>
2020-06-10 22:30:56 +0000

    updating to use the latest vistasoft, and matlab 2019a to see if we can reduce memory footprint

--- build.m
+++ build.m
@@ -2,6 +2,5 @@ addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/soft/mason/SPM/spm8'))
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/u/brlife/git/encode'))
-addpath(genpath('/N/u/brlife/git/fine'))
 mcc -m -R -nodisplay -d compiled main
 exit
--- compile.sh
+++ compile.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-module load matlab/2017a
+module unload matlab
+module load matlab/2019a
 
-mkdir -p compiled
+log=compiled/commit_ids.txt
+true > $log
 
-matlab -nodisplay -nosplash -r build.m
+echo "/N/u/brlife/git/jsonlab" >> $log
+(cd /N/u/brlife/git/jsonlab && git log -1) >> $log
+
+echo "/N/soft/mason/SPM/spm8" >> $log
+(cd /N/soft/mason/SPM/spm8 && git log -1) >> $log
+
+echo "/N/u/hayashis/git/vistasoft" >> $log
+(cd /N/u/hayashis/git/vistasoft && git log -1) >> $log
+
+echo "/N/u/brlife/git/encode" >> $log
+(cd /N/u/brlife/git/encode && git log -1) >> $log
+
+#echo "/N/u/brlife/git/fine" >> $log
+#(cd /N/u/brlife/git/fine && git log -1) >> $log
+
+cat > build.m <<END
+addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/soft/mason/SPM/spm8'))
+addpath(genpath('/N/u/brlife/git/jsonlab'))
+addpath(genpath('/N/u/brlife/git/encode'))
+mcc -m -R -nodisplay -d compiled main
+exit
+END
+
+matlab -nodisplay -nosplash -r build && rm build.m
